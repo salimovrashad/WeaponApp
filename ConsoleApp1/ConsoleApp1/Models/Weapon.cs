aabcb58c48f854add4fe38e5743170a116e5fe11
salimovrashad <selimovresad33@gmail.com>
2023-10-25 11:33:10 +0400

    add task

---
+++ ConsoleApp1/ConsoleApp1/Models/Weapon.cs
@@ -0,0 +1,93 @@
+﻿namespace ConsoleApp1.Models
+{
+	internal class Weapon
+	{
+		public int tutum = 40;
+		public int sayi = 40;
+		public float bossaniye = 80;
+		public string modu = "avtomatik";
+
+		public void Shoot()
+		{
+            if (sayi == 0)
+            {
+				Console.WriteLine("Gulle yoxdu...");
+            }
+            else
+            {
+				sayi = sayi - 1;
+				Console.WriteLine($"Atis edildi. {sayi} qeder gulle qaldi...");
+            }
+        }
+		public void Fire()
+		{
+            if (sayi == 0)
+            {
+                Console.WriteLine("Gulle yoxdu!!!");
+            }
+            else
+            {
+				Console.WriteLine($"Bosaltma saniyesi {(bossaniye/tutum)*sayi} saniyedir. ");
+				sayi = 0;
+				Console.WriteLine($"{sayi} gulle qaldi...");
+
+			}
+		}
+		public void GetNeededBulletCount()
+		{
+			int number = tutum - sayi;
+            Console.WriteLine($"{number} gulle lazimdi...");
+        }
+		public void Reload()
+		{
+			sayi = tutum;
+            Console.WriteLine("Daraq yeniden doldu...");
+        }
+		public void ChangeFireMode()
+		{
+            if (modu == "tekli")
+            {
+				modu = "avtomatik";
+            }
+            else
+            {
+				modu = "tekli";
+            }
+            Console.WriteLine($"Modu {modu} olaraq deyisildi...");
+        }
+		public void CreateWeapon()
+		{
+            Console.WriteLine("Gulle tutumunu daxil edin: ");
+			tutum = Convert.ToInt32(Console.ReadLine());
+            if (tutum < 0)
+            {
+				Console.WriteLine("Duz reqem daxil edin!!!");
+				return;
+			}
+
+			Console.WriteLine("Gulle sayini daxil edin: ");
+			sayi = Convert.ToInt32(Console.ReadLine());
+			if (sayi < 0)
+			{
+				Console.WriteLine("Duz reqem daxil edin!!!");
+				return;
+			}
+
+			Console.WriteLine("Gulle bosalma saniyesini daxil edin: ");
+			bossaniye = Convert.ToInt32(Console.ReadLine());
+			if (bossaniye < 0)
+			{
+				Console.WriteLine("Duz reqem daxil edin!!!");
+				return;
+			}
+
+			Console.WriteLine("Atis modunu daxil edin: ");
+			modu = Console.ReadLine();
+			if (!(modu == "tekli" || modu == "avtomatik"))
+			{
+				Console.WriteLine("Duz deyil!!!");
+				return;
+			}
+		}
+	}
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,49 @@
+﻿using ConsoleApp1.Models;
+
+namespace ConsoleApp1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Weapon weapon = new Weapon();
+            for (int i = 0; true; i++)
+            {
+				Console.WriteLine(" 0-Informasiya Almaq \n 1-Shoot \n 2-Fire \n 3-GetRemainBulletCount \n 4-Reload \n 5-ChangeFireMode \n 6-Proqramdan cixin... \n C-Silah yarat...");
+				string secim = Console.ReadLine();
+
+				switch (secim)
+				{
+					case "0":
+						Console.WriteLine($"Silahin tutumu: {weapon.tutum} \nSilahdaki gulle sayi: {weapon.sayi} \nModu: {weapon.modu}");
+						break;
+					case "1":
+						weapon.Shoot();
+						break;
+					case "2":
+						weapon.Fire();
+						break;
+					case "3":
+						weapon.GetNeededBulletCount();
+						break;
+					case "4":
+						weapon.Reload();
+						break;
+					case "5":
+						weapon.ChangeFireMode();
+						break;
+					case "c":
+						weapon.CreateWeapon();
+						break;
+					case "6":
+						return;
+					default:
+                        Console.WriteLine("Duz secim edin!!!");
+                        break;
+				}
+
+			}
+
+        }
+	}
+}
